843cc2b34ac217f49c1df504cf6b7cfde0296025
ezra <ezra@ezra-laptop.(none)>
2013-03-21 04:21:33 -0700

    empty rails app

---
+++ neefty/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neefty::Application.config.secret_token = '15ab997a30b3a45dcc1bccaad3e5a214adc4fc8b6e12fe16b97edfcb63317b9711cf0442a67a39b7687c1d0fa06499a50e75504b0238fd939f7314d13722bd32'
